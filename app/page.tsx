dbcc1c8bf3fa7f2f3a6c81c8f46a6f94ea9e11dc
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-18 01:03:38 +0530

    customized button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <Button variant={"dark"} size={"primary"}>
-        Get started now{" "}
+      <Button asChild variant={"nav_link"} size={"nav_link"}>
+        <Link href="#">Google</Link>
       </Button>
     </div>
   );
