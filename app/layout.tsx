ac69ef0b6b687325d05684ebb47ee9ad166ec2fa
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-18 00:19:18 +0530

    installed shadcn button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 
 const roboto = Roboto({
-  variable: "--font-geist-sans",
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -18,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${roboto.variable}  antialiased`}>{children}</body>
+    <html lang="en" className={roboto.className}>
+      <body className={` antialiased`}>{children}</body>
     </html>
   );
 }
