788e33b85e259d122eb2478ff89d1ad79746c5b6
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-18 02:35:50 +0530

    developed skills section

--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+import {
+  RiGithubFill,
+  RiNextjsFill,
+  RiReactjsFill,
+  RiTailwindCssFill,
+} from "react-icons/ri";
 
 const SkillSection = () => {
+  const iconList = [
+    { name: "Next.js", icon: RiNextjsFill },
+    { name: "React", icon: RiReactjsFill },
+    { name: "Tailwind CSS", icon: RiTailwindCssFill },
+    { name: "GitHub", icon: RiGithubFill },
+  ];
   return (
     <div>
       <div className="sub-topic">Skills</div>
       <div className="skill-grid">
-        <div className="skill-row">
-          <div className="skill w-25 h-25 rounded-full flex items-center justify-center border border-black"></div>
+        <div className="skill-row flex gap-4">
+          {iconList.map((item, index) => {
+            const ItemComponent = item.icon; // Declare the variable here
+            return (
+              <div
+                key={index}
+                className="skill w-23 h-23 rounded-full flex items-center justify-center border border-black"
+              >
+                <ItemComponent className="text-9xl" />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
