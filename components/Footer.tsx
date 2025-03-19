ea9f4c10a170b8784bb1ed2fef48d52a7e581220
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-03-19 15:48:18 +0530

    footer  mobile responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,10 @@ import ContactDock from "./ui/contactdock";
 const Footer = () => {
   return (
     <div className="min-w-screen bg-prime-black-3 text-prime-white h-[466px] flex flex-col items-center justify-center gap-4">
-      <div className="thank name-text">Thank You</div>
-      <div className="message sub-topic max-w-[531px] text-center">
+      <div className="thank lg:text-[164px] text-[96px] lg:leading-35 leading-none text-center font-medium uppercase">
+        Thank You
+      </div>
+      <div className="message lg:sub-topic lg:max-w-[531px] max-w-[75vw]  text-center">
         for visiting my portfolio. I’m always looking for new opportunities to
         learn and grow. Stay connected!
       </div>
